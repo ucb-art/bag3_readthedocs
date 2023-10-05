c09ff3c68a63bdf4331c7d7d2398f0c9aa1df551
FeliciaG <felicia_guo@berkeley.edu>
2023-10-04 21:45:47 -0700

    adding api

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,33 @@
 # Configuration file for the Sphinx documentation builder.
 
+import os
+import sys
+# sys.path.insert(0, os.path.abspath('../../src'))
+# # sys.path.insert(-1, os.path.abspath('../../src/bag/'))
+# sys.path.insert(-1, os.path.abspath('../../pybag'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../xbase/src'))
+
+# from glob import glob
+# from distutils.dir_util import copy_tree
+# import subprocess
+
+# currentfolder = os.path.abspath('./') # aka docs/
+# rootfolder = os.path.abspath('../')
+# tmpfolder = "{}/tmp".format(currentfolder)
+# globdir = '{}/repo-*/repo'.format(rootfolder)
+# subprocess.check_call(["mkdir", "-p", tmpfolder]) # make sure temporary folder exists 
+
+# all_sub_dir_paths = glob(globdir) # returns list of sub directory paths
+# for subdir in all_sub_dir_paths:
+#     print("subdir: {} to tmp folder: {}".format(subdir, tmpfolder))
+#     copy_tree(subdir, tmpfolder) # copy all folders to docs/tmp/repo/*
+
+# autoapi_dirs = [tmpfolder]
+# sys.path.insert(0, tmpfolder)
+# autoapi_python_use_implicit_namespaces = True
+
+
 # -- Project information
 
 project = 'BAG3++'
@@ -16,16 +44,36 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosectionlabel',
+    'sphinx.ext.napoleon',
     'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
+    'autoapi.extension'
 ]
 
+
+autoapi_dirs = ['../../bag/src/bag',
+                '../../bag3_testbenches/src/bag3_testbenches',
+                '../../bag3_analog/src/bag3_analog',
+                '../../bag3_digital/src/bag3_digital',
+                '../../bag3_magnetics/src/bag3_magnetics',
+                '../../xbase/src/xbase',   ]
+autoapi_python_use_implicit_namespaces = True
+autoapi_file_patterns = ['*.py']
+autoapi_options = [ 'undoc-members', 'private-members', 'show-inheritance', 'show-module-summary', 'special-members']
+suppress_warnings = ["autoapi"]
+
+# autosummary_generate = False
+# autodoc_mock_imports = ["pybag", "ruamel", "srr", "libpsf", "lark", "bag3_digital", 
+#                         "srr_python", "openmdao", "ocean", "dc", 'bag.tech', 'bag.interface.simulator'
+                        # ]
+
+exclude_patterns = ['_build', '_templates', "../../xbase/src/xbase/__init__.py"]
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
-
 templates_path = ['_templates']
 
 # -- Options for HTML output
@@ -34,3 +82,4 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
